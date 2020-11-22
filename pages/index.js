aef63723407c07524a7b1f70873750ec78c637fd
Lucas Almeida <lucas@w3mind.com.br>
2020-11-22 11:37:34 -0300

    started project

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div id="page">
+      <h1>JG Vieira</h1>
+    </div>
+  )
+}
