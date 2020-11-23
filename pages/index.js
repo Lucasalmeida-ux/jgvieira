084dbedc0f9537e97add640c163335e5d1f8735d
Lucas Almeida <lucas@w3mind.com.br>
2020-11-22 23:49:47 -0300

    retirando paragrafos

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,5 @@ export default function Home() {
   return (
     <div id="page">
       <h1>JG Vieira</h1>
-      <p>
-<<<<<<< HEAD
-        men
-=======
-        menu
->>>>>>> 95d871503174dd0db0778d4f251772d6093ecaaf
-      </p>
-      <p>papao</p>
-    </div>
   )
 }
