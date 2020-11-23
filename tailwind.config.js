ac1157595e34a5d5079892be195ca6360979db15
Lucas Almeida <lucas@w3mind.com.br>
2020-11-23 20:49:36 -0300

    versão do dia

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+    },
+    container: {
+      center: true,
+    },
     extend: {},
   },
   variants: {
