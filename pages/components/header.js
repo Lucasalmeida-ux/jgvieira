2722fa2d7dd29f8e7b09a9e3bf810726e9684681
Lucas Almeida <lucas@w3mind.com.br>
2020-11-24 19:21:35 -0300

    dev header

--- pages/components/header.js
+++ pages/components/header.js
@@ -1,15 +1,39 @@
 import Link from "next/link"
 
+const menuItens = [
+    {link: "/quem-somos", label: "Quem Somos", key: "quemsomos"},
+    {link: "/produtos", label: "Produtos e Serviços", key: "produtos"},
+    {link: "/contato", label: "Contato", key: "contato"},
+]
+
+const Logo = () => (
+    <Link href="/"><a>
+        <img src="image/jgvieiralogo.png" alt="Logo da JG Vieira" className="py-2" width={80}></img>
+    </a></Link>
+)
+
+const Menu = () => (
+    <nav className="p-2 flex itens-center">
+        <ul className="md:p-4">
+        { menuItens.map(function(item) {
+            return <li key={item.key} className="inline px-4 font-semibold">
+                <Link href={item.link} className="bg-black"><a className="bg-black">{item.label}</a></Link>
+                </li>
+        }) }
+        </ul>
+    </nav>
+)
+
+
+
 export default function Header() {
     return (
-        <nav className=" bg-gray-lightest">
-            <header className="container text-right">
-                <img src="image/jgvieiralogo.png" alt="Logo da JG Vieira" className="logo"></img>
-                <Link href="/"><a>Home</a></Link>
-                <Link href="/quem-somos"><a>Quem Somos</a></Link>
-                <Link href="/produtos"><a>Produtos e Serviços</a></Link>
-                <Link href="/contato"><a>Fale Conosco</a></Link>
+        <div className="bg-blue">
+
+            <header className="p-2 container max-w-sxl m-auto flex justify-between">
+                <Logo />
+                <Menu />
             </header>
-        </nav>
+        </div>
     )
 }
--- pages/components/layout.js
+++ pages/components/layout.js
@@ -3,6 +3,7 @@ import Header from "./header";
 export default function Layout({ children }) {
 return <div>
     <Header></Header>
+    <div className="container divide-x-3 divide-gray-300"></div>
     {children}
     </div>
 }
